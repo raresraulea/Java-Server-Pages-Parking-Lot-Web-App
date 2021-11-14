9b6fcab5000633605de5cfee4970e993abcfa2f0
Rares Raulea <raresraulea2000@gmail.com>
2021-11-14 12:21:34 +0200

    feat(users): add user EJB and user details POJO

---
+++ src/main/java/cokm/park/parkinglot/ejb/UserBean.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package cokm.park.parkinglot.ejb;
+
+import com.park.parkinglot.common.UserDetails;
+import com.park.parkinglot.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author a
+ */
+@Stateless
+public class UserBean {
+
+    private static final Logger LOG = Logger.getLogger(UserBean.class.getName());
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    public List<UserDetails> getAllUsers() {
+        LOG.info("getAllUsers");
+        
+        try {
+            List<User> users = (List<User>) em.createQuery("SELECT c FROM User c").getResultList();
+            return copyUsersToDetails(users);
+        } catch (Exception ex) {
+            throw new EJBException(ex);
+        }
+    }
+    
+    private List<UserDetails> copyUsersToDetails(List<User> users) {
+        List<UserDetails> detailsList = new ArrayList<>();
+        for(User user : users) {
+            UserDetails UserDetails = new UserDetails(
+                    user.getId(),
+                    user.getUsername(),
+                    user.getEmail(),
+                    user.getPosition()
+            );
+            detailsList.add(UserDetails);
+        }
+        
+        return detailsList;
+    }
+    
+    
+}
